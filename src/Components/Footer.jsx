f5df12fcba212f1b43ecca188759417358d476c7
Mahfuz Shakib <mahfuzshakib301@gmail.com>
2025-10-09 16:15:03 +0600

    footer responsiveness updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,60 +9,62 @@ const Footer = () => {
       <div className="bg-[#06091A] text-neutral-content">
         <Container>
           <div className="max-w-7xl mx-auto px-3 py-10">
-            <div className="grid gap-8 sm:gap-10 md:gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
-              <div>
+            <div className="lg:flex justify-between grid gap-8 sm:gap-10 md:gap-24 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 ">
+              <div className="max-w-60 mx-auto sm:mx-0">
                 <Link to="/">
-                  <div className="inline-flex items-center gap-2">
+                  <div className="flex justify-center sm:justify-start items-center gap-2 ">
                     <img src={logo} alt="logo" className="size-12" />
                     <span className="text-xl font-semibold">HERO.IO</span>
                   </div>
                 </Link>
-                <p className="text-gray-400 mt-4 text-sm">A modern, responsive app gallery built with React, Vite and Tailwind. Discover apps, view insights, and manage installs locally.</p>
+                <p className="text-gray-400 text-center sm:text-start mt-4 text-sm">A modern, responsive app gallery built with React, Vite and Tailwind. Discover apps, view insights, and manage installs locally.</p>
               </div>
 
-              <div className="">
-                <h3 className="text-white font-semibold mb-3">Quick Links</h3>
-                <ul className="space-y-2 text-gray-300">
-                  <li>
+              <div>
+                <h3 className="text-white text-center sm:text-start font-semibold mb-3">Quick Links</h3>
+                <ul className="footer-ul">
+                  <li className="px-2 first:pl-0 sm:px-0">
                     <Link to="/" className="hover:underline">Home</Link>
                   </li>
-                  <li>
+                  <li className="px-2 first:pl-0 sm:px-0">
                     <Link to="/apps" className="hover:underline">Apps</Link>
                   </li>
-                  <li>
+                  <li className="px-2 first:pl-0 sm:px-0">
                     <Link to="/myinstallation" className="hover:underline">My Installation</Link>
                   </li>
-                  <li>
+                  <li className="px-2 first:pl-0 sm:px-0">
                     <a href="https://hero-apps-301.netlify.app/" target="_blank" rel="noopener noreferrer" className="hover:underline">Live</a>
                   </li>
                 </ul>
               </div>
 
               <div>
-                <h3 className="text-white font-semibold mb-3">About Project</h3>
-                <ul className="space-y-2 text-gray-300">
-                  <li>
-                    <a href="/" className="hover:underline">Getting Started</a>
+                <h3 className="text-white text-center sm:text-start font-semibold mb-3">About Project</h3>
+                <ul className="footer-ul">
+                  <li className="px-2 first:pl-0 sm:px-0">
+                    <a href="https://github.com/mahfuz-shakib/assignment-08-hero-apps" target="_blank"  className="hover:underline">Repository</a>
                   </li>
-                  <li>
-                    <a href="/" className="hover:underline">Features</a>
+                  <li className="px-2 first:pl-0 sm:px-0">
+                    <a href="https://github.com/mahfuz-shakib/assignment-08-hero-apps#getting-started" target="_blank"  className="hover:underline">Getting Started</a>
                   </li>
-               
+                  <li className="px-2 first:pl-0 sm:px-0">
+                    <a href="https://github.com/mahfuz-shakib/assignment-08-hero-apps#features" target="_blank"  className="hover:underline">Features</a>
+                  </li>
                 </ul>
               </div>
 
               <div>
-                <h3 className="text-white font-semibold mb-3">Follow</h3>
-                <div className="flex flex-col gap-2 text-gray-300">
-                  <a href="https://github.com/mahfuz-shakib" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 hover:opacity-80">
+                <h3 className="text-white text-center sm:text-start font-semibold mb-3">Follow</h3>
+                <div className="text-gray-300 text-sm sm:text-base flex flex-wrap justify-center sm:justify-start gap-1 divide-x divide-gray-600 sm:flex-col sm:gap-2 sm:divide-x-0">
+                  <a href="https://github.com/mahfuz-shakib" target="_blank"  className="inline-flex items-center gap-1 sm:gap-2 hover:opacity-80 px-3 first:pl-0 sm:px-0">
                     <FaGithub aria-hidden="true" />
                     <span>GitHub</span>
                   </a>
-                  <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 hover:opacity-80">
+                  <a href="https://facebook.com" target="_blank"  className="inline-flex items-center gap-1 sm:gap-2 hover:opacity-80 px-3 first:pl-0 sm:px-0">
                     <FaFacebook aria-hidden="true" />
                     <span>Facebook</span>
                   </a>
-                  <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-2 hover:opacity-80">
+                  <a href="https://www.linkedin.com" target="_blank"  className="inline-flex items-center gap-1 sm:gap-2 hover:opacity-80 px-3 first:pl-0 sm:px-0">
                     <FaLinkedin aria-hidden="true" />
                     <span>LinkedIn</span>
                   </a>
